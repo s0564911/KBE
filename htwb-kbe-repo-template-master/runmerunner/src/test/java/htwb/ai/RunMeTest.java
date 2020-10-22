61cc04b920d8cb1f095ed1501c5c13ae59ec64e7
TrialAndForce <s0552582@htw-berlin.de>
2020-10-22 16:07:17 +0200

    added test & lineseperator

--- htwb-kbe-repo-template-master/runmerunner/src/test/java/htwb/ai/RunMeTest.java
+++ htwb-kbe-repo-template-master/runmerunner/src/test/java/htwb/ai/RunMeTest.java
@@ -8,19 +8,22 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RunMeTest {
-
+	
     private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalErr = System.err;
-
+    private final PrintStream originalOut = System.out;
     String[] args = new String[1];
 
     @BeforeEach
     public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
         System.setErr(new PrintStream(errContent));
     }
 
     @AfterEach
     public void restoreStreams() {
+        System.setOut(originalOut);
         System.setErr(originalErr);
     }
 
@@ -28,8 +31,8 @@ public class RunMeTest {
     public void couldNotFindClassTest() {
         args[0] = "blub";
         RunMeMain.main(args);
-        String testString = "Error: Could not find class blub\r\n" +
-                "Usage: java -jar runmerunner-KBE.jar className\r\n";
+        String testString = "Error: Could not find class blub" + System.lineSeparator() +
+                "Usage: java -jar runmerunner-KBE.jar className" + System.lineSeparator() ;
         assertEquals(testString, errContent.toString());
     }
 
@@ -37,8 +40,8 @@ public class RunMeTest {
     public void couldNotInstantiateClassTest() {
         args[0] = "java.lang.Number";
         RunMeMain.main(args);
-        String testString = "Error: Could not instantiate class java.lang.Number\r\n" +
-                "Usage: java -jar runmerunner-KBE.jar className\r\n";
+        String testString = "Error: Could not instantiate class java.lang.Number"+ System.lineSeparator() +
+                "Usage: java -jar runmerunner-KBE.jar className"+ System.lineSeparator();
         assertEquals(testString, errContent.toString());
     }
 
@@ -46,9 +49,9 @@ public class RunMeTest {
     public void couldNotFindConstructorTest() {
         args[0] = "java.io.PrintStream";
         RunMeMain.main(args);
-        String testString = "Error: Could not find constructor of class java.io.PrintStream\r\n" +
-                "Error: Could not instantiate class java.io.PrintStream\r\n" +
-                "Usage: java -jar runmerunner-KBE.jar className\r\n";
+        String testString = "Error: Could not find constructor of class java.io.PrintStream"+ System.lineSeparator() +
+                "Error: Could not instantiate class java.io.PrintStream"+ System.lineSeparator() +
+                "Usage: java -jar runmerunner-KBE.jar className"+ System.lineSeparator();
         assertEquals(testString, errContent.toString());
     }
 
@@ -56,9 +59,28 @@ public class RunMeTest {
     public void couldNotAccessConstructorTest() {
         args[0] = "htwb.ai.TestClassPrivateConstructor";
         RunMeMain.main(args);
-        String testString = "Error: Could not access constructor of class htwb.ai.TestClassPrivateConstructor\r\n" +
-                "Error: Could not instantiate class htwb.ai.TestClassPrivateConstructor\r\n" +
-                "Usage: java -jar runmerunner-KBE.jar className\r\n";
+        String testString = "Error: Could not access constructor of class htwb.ai.TestClassPrivateConstructor"+ System.lineSeparator() +
+                "Error: Could not instantiate class htwb.ai.TestClassPrivateConstructor" + System.lineSeparator()+
+                "Usage: java -jar runmerunner-KBE.jar className"+ System.lineSeparator();
         assertEquals(testString, errContent.toString());
     }
+    @Test
+    public void succesfulDefaultCaseTest() {
+    	args[0] = "htwb.ai.TestClass";
+    	RunMeMain.main(args);
+    	String testString = "Analyzed class 'htwb.ai.TestClass':"+ System.lineSeparator() + 
+    			"Methods without @RunMe: "+ System.lineSeparator() + 
+    			"  testWithoutRM"+ System.lineSeparator() + 
+    			"  testNoRM\n" + 
+    			"Methods with @RunMe: "+ System.lineSeparator() + 
+    			"  findMe0"+ System.lineSeparator() + 
+    			"  findMe2"+ System.lineSeparator() + 
+    			"  findMe4"+ System.lineSeparator() + 
+    			"not invocable:"+ System.lineSeparator() + 
+    			"  findMe3: IllegalAccessException"+ System.lineSeparator() + 
+    			"needing arguments to be run:"+ System.lineSeparator() + 
+    			"  findMe1";
+    	assertEquals(testString, outContent.toString());
+    }
+  
 }
