0f63d00a28f46d6f53ac94a18face51d55391c21
s0564911 <63096991+s0564911@users.noreply.github.com>
2021-01-21 20:24:25 +0100

    SongListControllerTest initial db fill

--- songsWS/src/test/java/de.htwb.ai.kbe/controller/SongListControllerTests.java
+++ songsWS/src/test/java/de.htwb.ai.kbe/controller/SongListControllerTests.java
@@ -3,6 +3,7 @@ package de.htwb.ai.kbe.controller;
 import de.htwb.ai.kbe.dao.SongDAO;
 import de.htwb.ai.kbe.dao.UserDAO;
 import de.htwb.ai.kbe.model.Song;
+import de.htwb.ai.kbe.model.SongList;
 import de.htwb.ai.kbe.model.User;
 import de.htwb.ai.kbe.service.SongService;
 import de.htwb.ai.kbe.service.UserService;
@@ -24,74 +25,99 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class SongListControllerTests {
-//    private static MockMvc mockMvc;
-//    private static Session session;
-//    private static Transaction tx;
-//    private static SessionFactory sessionFactory;
-//    private static String token;
-//    private static HttpHeaders headers;
-//
-//    private static final int currentID = 2;
-//
-//    // get all
-//
-//    @BeforeAll
-//    public static void setup() {
-//
-//        sessionFactory = new MetadataSources(
-//                new StandardServiceRegistryBuilder()
-//                        .configure()
-//                        .build()
-//        ).buildMetadata().buildSessionFactory();
-//
-//        UserService userService = new UserService(new UserDAO(sessionFactory));
-//
-//        mockMvc = MockMvcBuilders.standaloneSetup(
-//                new SongController(
-//                        new SongService(new SongDAO(sessionFactory)),
-//                        userService
-//                )).build();
-//
-//        session = sessionFactory.getCurrentSession();
-//        tx = session.beginTransaction();
-//
-//        User u = User.builder().withUserId("mmuster").withFirstname("Maxime").withLastname("Muster").withPassword("pass1234").build();
-//        session.save(User.builder().withUserId("eschuler").withFirstname("Elena").withLastname("Schuler").withPassword("pass1234").build());
-//
-//        session.save(u);
-//        session.save(Song.builder().withTitle("Straight Outta Compton").withArtist("N.W.A.").withLabel("Ruthless").withReleased(1988).build());
-//        session.save(Song.builder().withTitle("Gangsta Gangsta").withArtist("N.W.A.").withLabel("Ruthless").withReleased(1988).build());
-//        session.save(Song.builder().withTitle("Fuck tha Police").withArtist("N.W.A.").withLabel("Ruthless").withReleased(1988).build());
-//        tx.commit();
-//
-//        session = sessionFactory.getCurrentSession();
-//        tx = session.beginTransaction();
-//        token = userService.generateNewToken(u.getUserId(), u.getPassword());
-//        tx.commit();
-//
-//        headers = new HttpHeaders();
-//        headers.add("Authorization", token);
-//    }
-//
-//    @BeforeEach
-//    void beforeEach() {
-//        session = sessionFactory.getCurrentSession();
-//        tx = session.beginTransaction();
-//    }
-//
-//    @AfterEach
-//    void afterEach() {
-//        tx.commit();
-//    }
-//
-//    @AfterClass
-//    public static void closeTransaction() {
-//        session.close();
-//    }
+    private static MockMvc mockMvc;
+    private static Session session;
+    private static Transaction tx;
+    private static SessionFactory sessionFactory;
+    private static String token;
+    private static HttpHeaders headers;
+
+    private static final int currentID = 2;
+
+    // get all
+
+    @BeforeAll
+    public static void setup() {
+
+        sessionFactory = new MetadataSources(
+                new StandardServiceRegistryBuilder()
+                        .configure()
+                        .build()
+        ).buildMetadata().buildSessionFactory();
+
+        UserService userService = new UserService(new UserDAO(sessionFactory));
+
+        mockMvc = MockMvcBuilders.standaloneSetup(
+                new SongController(
+                        new SongService(new SongDAO(sessionFactory)),
+                        userService
+                )).build();
+
+        session = sessionFactory.getCurrentSession();
+        tx = session.beginTransaction();
+
+        User mmuster = User.builder().withUserId("mmuster").withFirstname("Maxime").withLastname("Muster").withPassword("pass1234").build();
+        User eschuler = User.builder().withUserId("eschuler").withFirstname("Elena").withLastname("Schuler").withPassword("pass1234").build();
+
+        Song song1 = Song.builder().withTitle("Straight Outta Compton").withArtist("N.W.A.").withLabel("Ruthless").withReleased(1988).build();
+        Song song2 = Song.builder().withTitle("Gangsta Gangsta").withArtist("N.W.A.").withLabel("Ruthless").withReleased(1988).build();
+        Song song3 = Song.builder().withTitle("Fuck tha Police").withArtist("N.W.A.").withLabel("Ruthless").withReleased(1988).build();
+        Song song4 = Song.builder().withTitle("Parental Discretion Iz Advised").withArtist("N.W.A.").withLabel("Ruthless").withReleased(1988).build();
+        Song song5 = Song.builder().withTitle("8 Ball (Remix)").withArtist("N.W.A.").withLabel("Ruthless").withReleased(1988).build();
+        Song song6 = Song.builder().withTitle("Something Like That").withArtist("N.W.A.").withLabel("Ruthless").withReleased(1988).build();
+
+        session.save(mmuster);
+        session.save(eschuler);
+        session.save(song1);
+        session.save(song2);
+        session.save(song3);
+        session.save(song4);
+        session.save(song5);
+        session.save(song6);
+        tx.commit();
+
+        session = sessionFactory.getCurrentSession();
+        tx = session.beginTransaction();
+        token = userService.generateNewToken(mmuster.getUserId(), mmuster.getPassword());
+        tx.commit();
+
+        headers = new HttpHeaders();
+        headers.add("Authorization", token);
+
+        SongList songList1 = SongList.builder().withOwnerid(mmuster).withIsPriv(true).withName("Liste1").withSongs(new HashSet<>(Arrays.asList(song4, song5))).build();
+        SongList songList2 = SongList.builder().withOwnerid(mmuster).withIsPriv(false).withName("Liste2").withSongs(new HashSet<>(Arrays.asList(song1, song2))).build();
+        SongList songList3 = SongList.builder().withOwnerid(eschuler).withIsPriv(true).withName("Liste3").withSongs(new HashSet<>(Arrays.asList(song3, song6))).build();
+        SongList songList4 = SongList.builder().withOwnerid(eschuler).withIsPriv(false).withName("Liste4").withSongs(new HashSet<>(Arrays.asList(song1, song4))).build();
+
+        session.save(songList1);
+        session.save(songList2);
+        session.save(songList3);
+        session.save(songList4);
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        session = sessionFactory.getCurrentSession();
+        tx = session.beginTransaction();
+    }
+
+    @AfterEach
+    void afterEach() {
+        tx.commit();
+    }
+
+    @AfterClass
+    public static void closeTransaction() {
+        session.close();
+    }
 
 
 }
